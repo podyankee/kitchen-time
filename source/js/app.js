e628c83a378e65972e50d9b70f39a81f5461f466
podyankee <ukon_2004@mail.ru>
2018-01-05 07:01:08 +0300

    working on header slider

--- source/js/app.js
+++ source/js/app.js
@@ -1,81 +1,25 @@
 $(document).ready(function(){
     $('.js-index-slider').slick({
+        prevArrow: '.index-slider__controls--prev',
+        nextArrow: '.index-slider__controls--next',
         dots: true,
-        vertical: true,
-        prevArrow: '.index-slider-controls--prev',
-        nextArrow: '.index-slider-controls--next',
-        customPaging : function() {
+        customPaging: function () {
             return '<a class="index-slider__dots"></a>';
-        },
+        }
     });
-    $('.js-recommended-slider').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        appendDots: '.recommended-slider__nav',
-        dots: true,
-        prevArrow: '.recommended-slider__controls--prev',
-        nextArrow: '.recommended-slider__controls--next',
-        customPaging : function() {
-            return '<a class="recommended-slider__dot"></a>';
-        },
-    });
-
-
     function indexSliderElemPos(elem, pos) {
         var windowWidth = $(window).width(),
             containerWidth = $('.container').width(),
             leftPos = (windowWidth - containerWidth)/2;
-        $('.index-slider ' + elem).css(pos, leftPos);
+            $('.index-slider ' + elem).css(pos, leftPos);
     }
     indexSliderElemPos('.slick-dots', 'left');
-    indexSliderElemPos('.index-slider-controls', 'right');
+    indexSliderElemPos('.index-slider__controls', 'right');
     $(window).resize(function () {
         indexSliderElemPos('.slick-dots', 'left');
-        indexSliderElemPos('.index-slider-controls', 'right');
+        indexSliderElemPos('.index-slider__controls', 'right');
+
+
     });
-
-    // Слайдер продукта
-
-    $('.js-product-gallery').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: true,
-        asNavFor: '.js-product-gallery-nav',
-        prevArrow: '.product-gallery__controls--prev',
-        nextArrow: '.product-gallery__controls--next'
     });
-    // Слайдер навигации продукта
-
-    $('.js-product-gallery-nav').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        dots: true,
-        centerMode: true,
-        focusOnSelect: true,
-        asNavFor: '.js-product-gallery'
-    });
-
-    // Tabs
-
-    $('.tabs-list__item').click(function () {
-        var tabName = $(this).attr('show-tab');
-        $(this).addClass('active').siblings().removeClass('active');
-        $('.tabs-content .' + tabName).addClass('active').siblings().removeClass('active');
-    });
-
     
-
-    // Video start
-
-    $('.video-start').click(function () {
-        var videoId = $(this).parent().attr('data-video-id'),
-            videoPlayer = $(this).parent().attr('id');
-        player = new YT.Player(videoPlayer, {
-           "autoplay" :1,
-            videoId: videoId
-            
-            
-        });
-    });
-
-});
